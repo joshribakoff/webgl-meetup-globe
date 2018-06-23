df79963332f0b2d0752cd250147de1e1d867c7be
Josh Ribakoff <josh.ribakoff@gmail.com>
2018-06-23 15:47:21 -0700

    fix missing key by mapping rsvp.id

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { webSocket } from "rxjs/webSocket";
+import { map } from "rxjs/operators";
 import styled from "styled-components";
 import "./App.css";
 
 const URL = "ws://stream.meetup.com/2/rsvps";
 
 const Card = styled.div`
-padding: 5px;
-margin: 5px;
-`
+  padding: 5px;
+  margin: 5px;
+`;
 
 const Rsvp = ({ member_name, member_photo, group_name, group_city }) => (
   <Card>
@@ -19,16 +20,22 @@ const Rsvp = ({ member_name, member_photo, group_name, group_city }) => (
   </Card>
 );
 
+let id = 0;
+
 class App extends Component {
   state = { rsvps: [] };
 
-  componentDidMount = () => webSocket(URL).subscribe(this.handleRsvp);
+  componentDidMount = () =>
+    webSocket(URL)
+      .pipe(map(rsvp => ({ ...rsvp, id: id++ })))
+      .subscribe(this.handleRsvp);
 
   handleRsvp = rsvp =>
     this.setState(state => ({ rsvps: [rsvp, ...state.rsvps] }));
 
-  renderCard = ({ member, group }) => (
+  renderCard = ({ id, member, group }) => (
     <Rsvp
+      key={id}
       member_photo={member.photo}
       member_name={member.member_name}
       group_name={group.group_name}
