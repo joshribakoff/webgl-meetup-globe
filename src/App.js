5a9c2221ee7d1209548b947dc2028e8e0797139b
Josh Ribakoff <joshribakoff@Joshs-MacBook-Pro.local>
2018-06-23 15:40:31 -0700

    add member photo

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ padding: 5px;
 margin: 5px;
 `
 
-// Todo - add avatar pic
-const Rsvp = ({ member_name, group_name, group_city }) => (
+const Rsvp = ({ member_name, member_photo, group_name, group_city }) => (
   <Card>
+    <img src={member_photo} />
     <strong>{member_name}</strong> will meetup with<br />
     <strong>{group_name}</strong>
     <br />in {group_city}
@@ -29,6 +29,7 @@ class App extends Component {
 
   renderCard = ({ member, group }) => (
     <Rsvp
+      member_photo={member.photo}
       member_name={member.member_name}
       group_name={group.group_name}
       group_city={group.group_city}
