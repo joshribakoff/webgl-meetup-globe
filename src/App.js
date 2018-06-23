3412212848bab75cb5e12cd4a21fe3304363daf4
Josh Ribakoff <joshribakoff@Joshs-MacBook-Pro.local>
2018-06-23 16:02:59 -0700

    avatar styling

--- src/App.js
+++ src/App.js
@@ -9,18 +9,39 @@ const URL = "ws://stream.meetup.com/2/rsvps";
 const Card = styled.div`
   padding: 5px;
   margin: 5px;
+  display: flex;
+`;
+
+const MemberPhotoWrapper = styled.div`
+  width: 70px;
+  flex-shrink: 0;
+  text-align: center;
+  img {
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 const MemberPhoto = ({ name, photo }) => (
-  <img src={photo} alt={`Avatar for ${name}`} />
+  <MemberPhotoWrapper>
+    {photo && <img src={photo} alt={`Avatar for ${name}`} />}
+  </MemberPhotoWrapper>
+);
+
+const RsvpDetails = ({ name, group, city }) => (
+  <div>
+    <strong>{name}</strong> will meetup with<br />
+    <strong>{group}</strong>
+    <br />
+    in {city}
+  </div>
 );
 
 const Rsvp = ({ member_name, member_photo, group_name, group_city }) => (
   <Card>
     <MemberPhoto name={member_name} photo={member_photo} />
-    <strong>{member_name}</strong> will meetup with<br />
-    <strong>{group_name}</strong>
-    <br />in {group_city}
+    <RsvpDetails name={member_name} group={group_name} city={group_city} />
   </Card>
 );
 
