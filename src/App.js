ba3d3dc6f62fd73e16c8d730c998bf16f79cecc7
Josh Ribakoff <joshribakoff@Joshs-MacBook-Pro.local>
2018-06-23 15:22:37 -0700

    renders json on page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import { webSocket } from 'rxjs/webSocket';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { webSocket } from "rxjs/webSocket";
+import "./App.css";
 
 class App extends Component {
+  state = { rsvps: [] };
+
   async componentDidMount() {
     const subject = webSocket("ws://stream.meetup.com/2/rsvps");
-    subject.subscribe(
-      value => console.log(value)
+    subject.subscribe(rsvp =>
+      this.setState(state => ({ rsvps: [...state.rsvps, rsvp] }))
     );
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div>{JSON.stringify(this.state.rsvps)}</div>;
   }
 }
 
